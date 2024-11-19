fa0076ffb64aff788d43985605186da5b33bab79
Mathieu Sevegny <msevegny@machinex.ca>
2024-11-18 20:16:12 -0500

    [Feat] Added actions and layout

---
+++ PersonalDeskThing.App/PersonalDeskThing.App.Client/Interfaces/IAction.cs
@@ -0,0 +1,8 @@
+﻿namespace PersonalDeskThing.App.Client.Interfaces
+{
+    public interface IAction
+    {
+        public Uri Image { get; set; }
+        public Task Launch();
+    }
+}
---
+++ PersonalDeskThing.App/PersonalDeskThing.App/Commands/LaunchApp.cs
@@ -0,0 +1,53 @@
+﻿using PersonalDeskThing.App.Client.Interfaces;
+using System.Diagnostics;
+
+namespace PersonalDeskThing.App.Commands
+{
+    public class LaunchApp : IAction
+    {
+        /// <summary>
+        /// Path to the executable
+        /// </summary>
+        public string ExecutablePath { get; set; }
+        /// <summary>
+        /// Image to display
+        /// </summary>
+        public Uri Image { get; set; }
+        /// <summary>
+        /// Launch the application
+        /// </summary>
+        /// <returns></returns>
+
+        public Task Launch()
+        {
+            if (string.IsNullOrEmpty(ExecutablePath))
+            {
+                throw new InvalidOperationException("Executable path is not set");
+            }
+            if (!File.Exists(ExecutablePath))
+            {
+                throw new InvalidOperationException("Executable path does not exist");
+            }
+            try
+            {
+                var p = new Process();
+                p.StartInfo = new ProcessStartInfo(ExecutablePath)
+                {
+                    UseShellExecute = true
+                };
+                p.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return Task.CompletedTask;
+        }
+        public LaunchApp(string executablePath, Uri image)
+        {
+            ExecutablePath = executablePath;
+            Image = image;
+        }
+    }
+}
